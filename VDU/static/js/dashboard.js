fffc21fdff89e90506fd6467f30ef6e716896d6b
Rui Mendes <rmendes@gowi-fi.pt>
2017-09-02 14:26:54 +0100

    waves sound

--- VDU/static/js/dashboard.js
+++ VDU/static/js/dashboard.js
@@ -104,20 +104,15 @@ $(document).ready(function () {
         //client.end()
     });
 
-    /********* EventSource **************/
-    /*
-    var source = new EventSource("{{ url_for('sse.stream') }}");
-    source.addEventListener('greeting', function(event) {
-            var data = JSON.parse(event.data);
-            //alert("The server says " + data.message);
-            eventOutputContainer.innerHTML = data.message;
-        }, false);
-        source.addEventListener('error', function(event) {
-            var msg = "Failed to connect to event stream. Is Redis running?";
-            console.log(msg);
-            eventOutputContainer.innerHTML = msg;
-    }, false);
-    */
 
-
+    var SW = new SiriWave({
+        width: 400,
+        height: 100,
+        speed: 0.12,
+        amplitude: 1,
+        container: document.getElementById('siri-container'),
+        autostart: true
+    });
 });
+
+
