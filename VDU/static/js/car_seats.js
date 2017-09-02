9516bbf873ba65ebd4432b464ea15d639b247250
Rui Mendes <rmendes@gowi-fi.pt>
2017-09-03 00:05:21 +0100

    stuff

--- VDU/static/js/car_seats.js
+++ VDU/static/js/car_seats.js
@@ -18,58 +18,62 @@ function vdu_setPersonInSeat(seat, id){
     var user = users[id];
 
     var img_height;
-    var w_weight, h_weight;
+    var w_weight, h_weight, l, t;
     switch(seat) {
         case 'FrontLeft':
             img_height = 80;
-            w_weight = 0.42;
-            h_weight = 0.60;
+            l = 181;
+            t = 1006;
             break;
         case 'FrontRight':
             img_height = 80;
-            w_weight = 0.42;
-            h_weight = 0.20;
+            l = 288;
+            t = 1006;
             break;
         case 'RearLeft':
             img_height = 65;
-            w_weight = 0.62;
-            h_weight = 0.62;
+            l = 185;
+            t = 1135;
             break;
         case 'RearRight':
             img_height = 65;
-            w_weight = 0.62;
-            h_weight = 0.18;
+            l = 310;
+            t = 1135;
             break;
         case 'RearCenter':
             img_height = 65;
-            w_weight = 0.62;
-            h_weight = 0.40;
+            l = 242;
+            t = 1135;
             break;
         default:
             w_weight = 0;
             h_weight = 0;
     }
 
-
-
     var image = document.getElementById('car-occupation-img');
     margin = 10;
 
+    /*
     l = image.offsetLeft;
     t = image.offsetTop;
     w = image.width;
     h = image.height;
 
+    console.log(l, t);
     // Location inside the image
     offX = parseInt(w_weight * w);
     offY = parseInt(h_weight * h);
 
     if(offX > margin) offX -= margin;
     if(offY > margin) offY -= margin;
-
     l += offX;
-    t += offY;
 
+    t += offY;
+    console.log(l, t);
+
+    l = 300;
+    t = 900;
+    */
     if (!firstTime[id]){
         var bubble = document.createElement("img");
         bubble.setAttribute('src', 'static/images/bubble.png');
